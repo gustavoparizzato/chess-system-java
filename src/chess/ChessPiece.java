eb242dc67a39735884f65352de9b5e5517a5f8cd
gustavoparizzato <gustavoventura246@gmail.com>
2022-02-07 21:04:12 -0300

    Moving pieces

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Piece;
 public class ChessPiece extends Piece{
 	
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -14,6 +15,18 @@ public class ChessPiece extends Piece{
 
 	public Color getColor() {
 		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
 	}
 
 }
